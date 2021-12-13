510ca8dc4864a22ca42376bef93ad51a3870a487
gabij <gabi.jospin@gmail.com>
2021-12-13 11:15:14 -0300

    Corrigindo __init__.py

--- src/queries/__init__.py
+++ src/queries/__init__.py
@@ -1,1 +1,6 @@
 from .animal import Animal
+from .dieta import Dieta
+from .dono import Dono
+from .endereco import Endereco
+from .medicacao import Medicacao
+from .relacaoMedicamentos import RelacaoMedicamentos
